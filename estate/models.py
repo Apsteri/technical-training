81b18e499c727654306d10de2219c638a1887c58
Apsteri <32962405+Apsteri@users.noreply.github.com>
2022-10-10 12:19:57 +0300

    Working

--- estate/models.py
+++ estate/models.py
@@ -1,7 +1,13 @@
-from odoo import models
-class TestModel(models.Model):
-    _name = "test.model"
+from odoo import models, fields
+
+
+class estate_property(models.Model):
+    _name = "estate.property"
     _description = "Estate test model for Odoo"
+    _order = "sequence"
 
-
-    
+    name = fields.Char('Name', required=True, translate=True)
+    description = fields.Text()
+    postcode = fields.Char('Postcode', required=True)
+    date_availability = fields.Date('date', required=True)
+    expected_price = fields.Float()
