aacfb996999f8de59b53144efa847b5857dba2f9
Apsteri <32962405+Apsteri@users.noreply.github.com>
2022-10-11 12:09:15 +0300

    Version 18

--- estate/models.py
+++ estate/models.py
@@ -7,17 +7,17 @@ class estate_property(models.Model):
     
 
     name = fields.Char('Name', required=True, translate=True)
-    description = fields.Text()
+    description = fields.Text('Description')
     postcode = fields.Char('Postcode', required=True)
     date_availability = fields.Date('date', required=True)
     expected_price = fields.Float()
     selling_price = fields.Float('Selling price', readonly=True, copy=False)
     bedrooms = fields.Integer('# Rooms', required=True)
-    living_area = fields.Integer('# living area (m^2)', required=True)
+    living_area = fields.Integer('# living area', required=True)
     facades = fields.Integer()
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('# garden area (m^2)')
+    garden_area = fields.Integer('# garden area')
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north','North'),('south','South'),('east','East'),('west','West')]
